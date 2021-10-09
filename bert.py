cc69737c1952cf5e2e2ed830a88f7b1043c2d692
nico_yang@126.com <ycy210921>
2021-10-09 15:07:49 +0800

    changes

--- bert.py
+++ bert.py
@@ -11,12 +11,12 @@ class Config(object):
         self.class_list = [x.strip() for x in open(path + '/class.txt').readlines()]
         self.save_path = path + '/bert.pt'
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.require_improvement = 2000
+        self.require_improvement = 1000
         self.num_classes = len(self.class_list)
-        self.num_epochs = 100
+        self.num_epochs = 3
         self.batch_size = 128
-        self.pad_size = 103  # 添加['cls']
-        self.learning_rate = 5e-5
+        self.pad_size = 99  # 添加['cls']
+        self.learning_rate = 2e-5
         self.bert_path = './bert_model'
         self.tokenizer = BertTokenizer.from_pretrained(self.bert_path)
         self.hidden_size = 768
@@ -29,13 +29,14 @@ class Model(nn.Module):
         for param in self.bert.parameters():
             param.requires_grad = True
         self.fc = nn.Linear(config.hidden_size, config.num_classes)
+        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
         context = x[0]
         mask = x[2]
         _, pooled = self.bert(context, attention_mask=mask, return_dict=False)
         output = self.fc(pooled)
-        return output
+        return self.softmax(output)
 
 
 
---
+++ predict.py
@@ -0,0 +1,54 @@
+import argparse
+
+import torch
+from transformers import BertTokenizer
+from bert import Model
+from importlib import import_module
+
+parser = argparse.ArgumentParser(description='Chinese test classification')
+parser.add_argument('--model', type=str, required=True, help='choose a model: Bert')
+args = parser.parse_args(['--model', 'bert'])
+
+PAD, CLS = '[PAD]', '[CLS]'  # padding符号, bert中综合信息符号
+
+
+def deal_message(message, config):
+    token = config.tokenizer.tokenize(message)
+    token = [CLS] + token
+    seq_len = len(token)
+    mask = []
+    token_id = config.tokenizer.convert_tokens_to_ids(token)
+    pad_size = 99
+    if pad_size:
+        if len(token) < pad_size:
+            mask = [1] * len(token_id) + [0] * (pad_size - len(token))
+            token_id += ([0] * (pad_size - len(token)))
+        else:
+            mask = [1] * pad_size
+            token_id = token_id[:pad_size]
+            seq_len = pad_size
+    token_id = torch.LongTensor([token_id]).to(config.device)
+    seq_len = torch.LongTensor([seq_len]).to(config.device)
+    mask = torch.LongTensor([mask]).to(config.device)
+    return token_id, seq_len, mask
+
+
+if __name__ == '__main__':
+    message = input('请输入一句话：')
+    path = './data'
+    model_name = args.model
+    x = import_module(model_name)
+    config = x.Config(path)
+    model = x.Model(config).to(config.device)
+    model.load_state_dict(torch.load(config.save_path))
+    model.eval()
+    context = deal_message(message, config)
+
+    res = model(context)
+    a = res.size()
+    res_dict = {0: 'negative', 1: 'positive'}
+    result_index = int(torch.argmax(res, 1)[0])
+    result_score = float(torch.max(res.data, 1)[0])
+    label = res_dict[result_index]
+    output = {'intent': label, 'confidence': result_score}
+    print(output)
